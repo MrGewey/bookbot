90e611d4a17698cbf4a1346c86d65a10a2b686b1
mrgewey <bryannikolasjones@gmail.com>
2025-02-22 23:52:30 +1100

    almost completed project. added report function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import count_words
+from stats import count_all
+from stats import report
 def main():
     #Type in the book. It will convert to lowercase, and check the books/ directory for the book.
     '''which_book: str = input("Enter the Book's name: ")
@@ -8,17 +11,11 @@ def main():
         file_contents = f.read()
         print(len(file_contents.split()))
     return file_contents'''
-    with open("books/frankenstein.txt") as f:
-        file_contents: str = f.read()
-        print(len(file_contents.split()))
+    
 
-#from stats import count_all
-
-        
-
-            
-
+    #print(f"{count_words()} words found in the document")
+    #print(count_all())
+    print(report())
 
 
 main()
-#count_all()
--- stats.py
+++ stats.py
@@ -1,14 +1,40 @@
-
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents: str = f.read()
+        word_count = (len(file_contents.split()))
+        return word_count
 
 def count_all():
-    total = {}
-    i = 0
+    total = {
+        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 
+        'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
+        }
     with open("books/frankenstein.txt") as book:
         booky: str = book.read()
         lower_book: str = booky.lower()
         words: str = lower_book.split()
         for word in words:
-            split_words = word.split()
-            for character in split_words:
-                slice_word = character[:]
-                print(slice_word)
+            for character in word:
+                if character in total:
+                    total[character] += 1
+                else:
+                    1 == 1
+    return total
+
+def report():
+
+    tally = count_all()
+    
+    sorted_tally = sorted(tally.items(), key = lambda x:x[1], reverse = True)
+    converted_dict = dict(sorted_tally)
+    
+       
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words()} total words")
+    print("--------- Character Count -------")
+    for letter in converted_dict:
+        print(f"{letter}: {converted_dict[letter]}")
+    print("============= END ===============")
